a3decf8d75217a373b5beeac5a9c8835fcfcbc48
wasifsn <wasif4000.wn@gmail.com>
2020-03-17 12:28:05 +0530

    add routes and intergrate with ui

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/api/register", function(req, res, next) {
+  let data = req.body;
+  console.log(data);
+  res.json(data);
+});
+
+router.post("/api/login", function(req, res, next) {
+  let data = req.body;
+  console.log(data);
+  res.json(data);
 });
 
 module.exports = router;
